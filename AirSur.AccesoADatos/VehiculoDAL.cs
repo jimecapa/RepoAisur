da506fc1360cae978ab2fd61f1cb715f7aa08247
Andres Velasquez <andres.3801920658@ucaldas.edu.co>
2022-05-10 19:51:43 -0500

    Creacion Clase Tipo de Vehiculo

---
+++ AirSur.AccesoADatos/VehiculoDAL.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AirSur.AccesoADatos
+{
+    public class VehiculoDAL
+    {
+        bdaisurEntities dbconexion = new bdaisurEntities();
+
+        public bool guardar(vehiculo vehiculo)
+        {
+            dbconexion.vehiculo.Add(vehiculo);
+            dbconexion.SaveChanges();
+            return true;
+        }
+
+        public List<vehiculo> mostrarVehiculos()
+        {
+            var query = (from s in dbconexion.vehiculo select s);
+            return query.ToList();
+        }
+    }
+}
---
+++ AirSur.EntidadesDeNegocio/Vehiculo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AirSur.EntidadesDeNegocio
+{
+    public class Vehiculo
+    {
+        [Key]
+        public long Id_vehiculo { get; set; }
+        public string Nombre { get; set; }
+        public int Cilindraje { get; set; }
+        public int CapacidadCargaMunicion { get; set; }
+        //[ForeignKey("TipoVehiculo")]
+        public long IdTipoVehiculo_Fk { get; set; }
+        //[ForeignKey("Marca")]
+        public long IdMarca_FK { get; set; }
+    }
+}
