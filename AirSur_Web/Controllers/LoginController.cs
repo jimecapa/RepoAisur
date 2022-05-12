a23304a7a3f6b2da7cfc90396d8f03edac82b53b
Jorge <jorge.3801721112@ucaldas.edu.co>
2022-05-11 22:17:20 -0500

    Implementacion de Login Falta generar conexion

---
+++ AirSur_Web/Controllers/LoginController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace AirSur_Web.Controllers
+{
+    public class LoginController : Controller
+    {
+
+
+
+        public ActionResult Index(bool? login)
+        {
+            if (login == true)
+            {
+                login = null;
+                return View();
+
+            }
+            return RedirectToAction("Login");
+        }
+
+        public string LoginUsuario(string user, string password)
+        {
+            var usuario = (from u in baseDatos.Login
+                           where u.Usuario.Equals(user) && u.Password.Equals(password)
+                           select u);
+
+
+            if (usuario != null && usuario.Any())
+            {
+                FormsAuthentication.SetAuthCookie(usuario.FirstOrDefault().Usuario, false);
+                ViewBag.username = usuario.Select(x => x.Usuario);
+                return Newtonsoft.Json.JsonConvert.SerializeObject(usuario);
+            }
+            else
+                return null;
+        }
+
+
+
+        public string Logout()
+        {
+            FormsAuthentication.SignOut();
+            return Newtonsoft.Json.JsonConvert.SerializeObject("Sesión cerrada con éxito");
+        }
+
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+
+
+
+    }
+}
---
+++ AirSur_Web/Scripts/Login.js
@@ -0,0 +1,34 @@
+﻿function LoguearUsuario() {
+    $.ajax({
+        type: 'post',
+        url: '/Home/LoginUsuario',
+        data: { user: $('#txtUsuario').val(), password: $('#txtPassword').val() },
+        dataType: 'json',
+        success: function (data) {
+            if (data[0]?.id) {
+                window.location.href = '/Home/Index?login=' + true;
+            }
+        },
+        error: function () {
+            alert('Login Erroneo');
+
+        }
+    });
+}
+
+
+function CerrarUsuario() {
+    $.ajax({
+        type: 'post',
+        url: '/Home/Logout',
+        dataType: 'json',
+        success: function (data) {
+            alert(data);
+            window.location.href = '/Home/Login';
+        },
+        error: function () {
+            alert('Se cerro incorrectamente');
+
+        }
+    });
+}
